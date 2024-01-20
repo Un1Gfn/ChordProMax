4ef02ff8af8d69ac36e53ac44a4d7a84258710ee
Darren Ng <Un1gfn@gmail.com>
2024-01-20 20:15:10 +0800

    venv

--- flask.zsh
+++ flask.zsh
@@ -2,6 +2,10 @@
 
 cd ~/ChordProMax
 
+if [[ Darwin == `uname -s` ]]; then
+  source venv/bin/activate
+fi
+
 eval "$($HOMEBREW_PREFIX/bin/perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5)"
 
 export CHORDPRO=$HOME/perl5/bin/chordpro
